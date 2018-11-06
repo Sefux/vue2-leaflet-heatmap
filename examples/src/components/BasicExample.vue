f854e6e9d409c975cfe0d1ede49aa7d2a630602e
Jurian Baas <jurian.baas@gmail.com>
2018-11-06 10:12:10 +0100

    Added extra lat lng points in array, added props to example component

--- examples/src/components/BasicExample.vue
+++ examples/src/components/BasicExample.vue
@@ -2,7 +2,7 @@
     <div class="basic-example">
         <l-map style="height: 100%; overflow: auto;" :zoom="10" :center="[47.334852, -1.509485]">
             <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
-            <Vue2LeafletHeatmap :latlngs="latlngs" :radius="500"></Vue2LeafletHeatmap>
+            <Vue2LeafletHeatmap :lat-lng="latlngs" :radius="60" :min-opacity=".75" :max-zoom="10" :blur="60"></Vue2LeafletHeatmap>
         </l-map>
     </div>
 </template>
@@ -20,10 +20,13 @@ export default {
   data() {
     return {
       latlngs: [
-        [47.334852, -1.509485, 0.2],
-        [47.342596, -1.328731, 0.3],
-        [47.241487, -1.190568, 0.4],
-        [47.234787, -1.358337, 0.5]
+        [47.334852, -1.509485, 1],
+        [47.342596, -1.328731, 0.75],
+        [47.342596, -1.329731, 1],
+        [47.342596, -1.329731, 1],
+        [47.342596, -1.329731, 1],
+        [47.241487, -1.190568, 0.5],
+        [47.234787, -1.358337, 1]
       ]
     };
   }
